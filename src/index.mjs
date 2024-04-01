5e06ac87fcfbbedc1e7e995cbd110710bdef3ae6
JameN9 <jamedas3654@gmail.com>
2024-04-01 13:46:08 +0700

    PUT and PATCH และการทำ destructuring เพื่อนำ property มาใช้งาน

--- src/index.mjs
+++ src/index.mjs
@@ -1,35 +1,52 @@
-import express, { response } from "express";
+import express, { json, response } from "express";
 import fetch from "node-fetch";
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(express.json());
 
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.status(201).json({ products });
 });
 
-//route
 const URL = `https://jsonplaceholder.typicode.com/users`;
+//route
 app.get("/api/users", async (req, res) => {
-    try {
-      const response = await fetch(URL);
-      const json = await response.json();
-     
-      const {query: { filter, value} } = req; //ใช้หลักการ destructuring  โดย re-name property ของ query  เป็น filter,value ซึ่งเป็น keyทั้งคู่
-      console.log(req.query);
-    
-      if(!filter && !value ){
-          return res.send(json);
-        }else if(filter && value ){
-            return res.send(json.filter((user) => user[filter].includes(value))) ; //เข้าถึง propertyโดยใช้ [] และใช้ .includes()เช็คค่าถ้ามีจะส่ง  ture ถ้าไม่จะส่ง false 
-        }
-        return res.send(json);
-    } catch (error) {
-      console.error(error);
-      res.status(500).send("Error query data");
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+
+    const {
+      query: { filter, value },
+    } = req; //ใช้หลักการ destructuring  โดย re-name property ของ query  เป็น filter,value ซึ่งเป็น keyทั้งคู่
+    console.log(req.query);
+
+    if (!filter && !value) {
+      return res.send(json);
+    } else if (filter && value) {
+      return res.send(json.filter((user) => user[filter].includes(value))); //เข้าถึง propertyโดยใช้ [] และใช้ .includes()เช็คค่าถ้ามีจะส่ง  ture ถ้าไม่จะส่ง false
     }
-  });
+    return res.send(json);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error query data");
+  }
+});
+
+app.post("/api/users", async (req, res) => {
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    const { body } = req;
+    const newUser = { id: json[json.length - 1].id + 1, ...body }; //id:json [เข้าถึง array ข้อมูลตัวสุดท้ายด้วยวิธี -1].id +1เมื่อมีค่าเพิ่มเข้ามา,...body คือเก็บค่าที่เพิ่มเข้ามาเช่น name:'',username:''
+    json.push(newUser);
+    console.log(newUser);
+    return res.status(201).send(newUser);
+  } catch (error) {
+    res.status(500).send({ msg: "Not found" });
+  }
+});
 
 app.get("/api/users/:id", async (req, res) => {
   const userId = parseInt(req.params.id);
@@ -49,12 +66,50 @@ app.get("/api/users/:id", async (req, res) => {
     }
     res.json(json);
   } catch (error) {
-    console.error(error);
     res.status(500).send("Error fetching data");
   }
 });
 
+app.put("/api/users/:id", async (req, res) => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  const {
+    body,
+    params: { id },
+  } = req;
 
+  const userId = parseInt(id);
+  if(isNaN(userId)){
+    return res.sendStatus(400);
+  }
+  const findUserIndex = json.findIndex((user) => user.id === userId);// userId = 2 findUserIndex = 1 (เพราะ index 1 มี id = 2)
+  if(findUserIndex === -1){
+    return res.sendStatus(404); 
+  }
+  json[findUserIndex] = { id: userId,...body };
+  console.log(json[findUserIndex] = { id: userId,...body })
+  return res.sendStatus(200);
+});
+
+app.patch("/api/users/:id", async (req, res) => {
+  const response = await fetch(URL);
+  const json = await response.json();
+  const {
+    body,
+    params: { id },
+  } = req;
+
+  const userId = parseInt(id);
+  if(isNaN(userId)){
+    return res.sendStatus(400);
+  }
+  const findUserIndex = json.findIndex((user) => user.id === userId);// userId = 2 findUserIndex = 1 (เพราะ index 1 มี id = 2)
+  if(findUserIndex === -1){
+    return res.sendStatus(404);
+  }
+  json[findUserIndex] = { ...json[findUserIndex],...body }; //{...json[index]}แสดงค่าใน indexที่กำหนด ,...body ส่งค่าที่จะอัพเดทเข้าไปใน index
+  return res.sendStatus(200);
+});
 
 app.listen(PORT, () => {
   console.log(`localhost:${PORT}`);
