0631cccaad35a4f0f9c4be5c15e36ef33b14281c
JameN9 <jamedas3654@gmail.com>
2024-04-25 12:44:31 +0700

    ผ่านการทำ middleware

--- src/index.mjs
+++ src/index.mjs
@@ -6,29 +6,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 //middleware
-const loggingMiddleware = (req, res, next) => {
-  console.log(`${req.method} - ${req.url}`);
-  next();
-};
-
 const resloveIndexByUserId = async (req, res, next) => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  const {
-    params: { id },
-  } = req;
-
-  const userId = parseInt(id);
-  if (isNaN(userId)) {
-    return res.sendStatus(400);
+  try {
+    const response = await fetch(URL);
+    const json = await response.json();
+    const {
+      params: { id },
+    } = req;
+    const userId = parseInt(id);
+    if (isNaN(userId)) {
+      return res.sendStatus(400);
+    }
+    const findUserIndex = json.findIndex((user) => user.id === userId); // userId = 2 findUserIndex = 1 (เพราะ index 1 มี id = 2) นับแบบ index arry
+    if (findUserIndex === -1) {
+      return res.sendStatus(404);
+    }
+    req.findUserIndex = findUserIndex; //ส่ง obj req ไปยัง app.put
+    req.json = json;
+    next();
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data");
+    next();
   }
-  const findUserIndex = json.findIndex((user) => user.id === userId); // userId = 2 findUserIndex = 1 (เพราะ index 1 มี id = 2)
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
-  req.findUserIndex = findUserIndex; //ส่ง obj req ไปยัง app.put
-  req.json = json;
-  next();
 };
 
 const PORT = process.env.PORT || 3000;
@@ -39,8 +39,8 @@ app.get("/", async (req, res) => {
   try {
     const response = await fetch(URL);
     const json = await response.json();
-    return res.status(201).json({ json });
-  } catch {
+    return res.send(json);
+  } catch (error) {
     console.error(error);
     res.status(500).send("Error query data");
   }
@@ -77,77 +77,62 @@ app.post("/api/users", async (req, res) => {
     json.push(newUser);
     return res.status(201).send(newUser);
   } catch (error) {
+    console.error(error);
     res.status(500).send({ msg: "Not found" });
   }
 });
 
-app.get("/api/users/:id", async (req, res) => {
-  const userId = parseInt(req.params.id);
-  if (isNaN(userId)) {
-    return res.status(400).send({ msg: "Bad request" });
-  }
+app.get("/api/users/:id", resloveIndexByUserId, async (req, res) => {
   try {
-    const response = await fetch(`${URL}/${userId}`);
-    const json = await response.json();
-    const toArray = [json] //เป็นเพราะ API ฝั่งนู้นนน ทำให้ต้องทำเป็น Array
-    console.log(toArray)
-    const foundUser = toArray.find((user) => user.id === userId);
+    const { findUserIndex, json } = req;
+    const foundUser = json[findUserIndex];
+    // const response = await fetch(`${URL}/${userId}`);
+    // const json = await response.json();
+    // const jsonToArray = [json]; //เป็นเพราะ API ฝั่งนู้นนน ทำให้ต้องทำเป็น Array
+    // const foundUser = jsonToArray.find((user) => user.id === userId);
     if (!foundUser) {
       return res.status(404).send({ msg: "Not Found" });
     } else {
       return res.send(foundUser);
     }
   } catch (error) {
+    console.error(error);
     res.status(500).send("Error fetching data");
   }
 });
 
 app.put("/api/users/:id", resloveIndexByUserId, async (req, res) => {
-  const { body, findUserIndex, json } = req;
-  json[findUserIndex] = { id: json[findUserIndex].id, ...body }; //แก้โดยการส่งค่า obj ผ่าน req 
-  console.log(json[findUserIndex]);
-  return res.sendStatus(200);
+  try {
+    const { body, findUserIndex, json } = req;
+    json[findUserIndex] = { id: json[findUserIndex].id, ...body }; //แก้โดยการส่งค่า obj ผ่าน req
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error feching data");
+  }
 });
 
-app.patch("/api/users/:id", async (req, res) => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  const {
-    body,
-    params: { id },
-  } = req;
-  const userId = parseInt(id);
-  if (isNaN(userId)) {
-    return res.sendStatus(400);
+app.patch("/api/users/:id", resloveIndexByUserId, async (req, res) => {
+  try {
+    const { body, findUserIndex, json } = req;
+    json[findUserIndex] = { ...json[findUserIndex], ...body }; //{...json[index]}แสดงค่าใน indexที่กำหนด ,...body ส่งค่าที่จะอัพเดทเข้าไปใน index
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data");
   }
-  const findUserIndex = json.findIndex((user) => user.id === userId); // userId = 2 findUserIndex = 1 (เพราะ index 1 มี id = 2)
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
-  json[findUserIndex] = { ...json[findUserIndex], ...body }; //{...json[index]}แสดงค่าใน indexที่กำหนด ,...body ส่งค่าที่จะอัพเดทเข้าไปใน index
-  return res.sendStatus(200);
 });
 
 app.delete("/api/users/:id", async (req, res) => {
-  const response = await fetch(URL);
-  const json = await response.json();
-  const jsonToArray = [json];
-  const {
-    params: { id },
-  } = req;
-
-  const userId = parseInt(id);
-
-  if (isNaN(userId)) {
-    return res.sendStatus(400);
+  try {
+    const { findUserIndex, json } = req;
+    const josnToArray = [json];
+    josnToArray.splice(findUserIndex); //ศึกษาเพิ่มเกี่ยวกับ splice slice
+    return res.sendStatus(200);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error fetching data");
   }
-
-  const findUserIndex = json.findIndex((user) => user.id === userId);
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
-  jsonToArray.splice(findUserIndex); //ศึกษาเพิ่มเกี่ยวกับ splice slice
-  return res.sendStatus(200);
 });
 
 //server
