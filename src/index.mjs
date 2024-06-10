4e58e22ea937e9c88671a4fa51fd6afa60953732
JameN9 <jamedas3654@gmail.com>
2024-06-10 13:01:13 +0700

    ทำmiddleware

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,24 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+//Middleware
+const resolveIndexByUserId = (req, res, next) => {
+  const {
+    params: { id },
+  } = req;
+  const parsedId = parseInt(id);
+  if (isNaN(parsedId)) {
+    return res.sendStatus(400);
+  }
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+  if (findUserIndex === -1) {
+    return res.sendStatus(404);
+  }
+  req.findUserIndex = findUserIndex;
+  next();
+};
+
 app.use(express.json());
 
 const mockUsers = [
@@ -52,43 +70,22 @@ app.get("/api/users/:id", (req, res) => {
   return res.send(findeUser);
 });
 
+
 //PUT Requests
-app.put("/api/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.status(400).send({ msg: "Bad Request. Invalid ID." });
-  }
-
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
-
-  //id : parsedId กำหนด id เพื่อไม่ให้ id หายไป โดยดึงค่าจาก parsedId ซึ่งรับค่าจาก req ผู้ใช้;
-  mockUsers[findUserIndex] = { id: parsedId, ...body };
+app.put("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req;
+  /**
+   * id: mockUsers[findUserIndex].id ไว้อ้างถึง obj id เพื่อไม่ให้ค่า id เปลี่ยนแปลงจากการอัพเดทจาก method put  (เดิมใช้ id: parsedId เพื่อเก็บค่าไอดีไว้)
+   * อันใหม่ใช้ id:  mockUsers[findUserIndex].id จัดการโดย middleware resolveIndexByUserId
+   * เนื่องจาก parsedId นำไปเทียบค่าใน findUserIndex แล้วได้ค่าตรงตามที่ต้องการจึงใช้ mockUsers[findUserIndex].id แทนและเข้าถึง obj id ได้เลย
+   */
+  mockUsers[findUserIndex] = { id: mockUsers[findUserIndex].id, ...body };
   return res.sendStatus(200);
 });
 
 //Patch Requests
-app.patch("/api/users/:id", (req, res) => {
-  const {
-    body,
-    params: { id },
-  } = req;
-
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.status(400).send({ msg: "Bad Request. Invalid ID." });
-  }
-
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
+app.patch("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { body, findUserIndex } = req;
 
   //...mockUsers[findUserIndex] เป็นการกระจายข้อมูลเดิม
   mockUsers[findUserIndex] = { ...mockUsers[findUserIndex], ...body };
@@ -96,23 +93,12 @@ app.patch("/api/users/:id", (req, res) => {
 });
 
 //Delete Requests
-app.delete("/api/users/:id", (req, res) => {
-  const {
-    params: { id },
-  } = req;
-  const parsedId = parseInt(id);
-  if (isNaN(parsedId)) {
-    return res.sendStatus(400);
-  }
-
-  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-  if (findUserIndex === -1) {
-    return res.sendStatus(404);
-  }
-
+app.delete("/api/users/:id", resolveIndexByUserId, (req, res) => {
+  const { findUserIndex } = req;
   mockUsers.splice(findUserIndex, 1);
   return res.sendStatus(200);
 });
+
 //server
 app.listen(PORT, () => {
   console.log(`localhost:${PORT}`);
