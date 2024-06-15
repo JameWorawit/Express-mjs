83d464b827851beea8f99b0cf25f1a10e188a00f
JameN9 <jamedas3654@gmail.com>
2024-06-15 17:42:55 +0700

    Test 3

--- src/index.mjs
+++ src/index.mjs
@@ -14,7 +14,7 @@ app.get("/", (req, res) => {
   res.status(201).send((mockUsers[0] = { ...body }));
 });
 
-//server please use .env
+//server please use .env !!
 app.listen(PORT, () => {
   console.log(`localhost:${PORT}`);
 });
