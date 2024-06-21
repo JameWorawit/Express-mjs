fd43d2bb16b41ed03e7c03f19b629b207d90ff07
JameN9 <jamedas3654@gmail.com>
2024-06-21 15:52:39 +0700

    ทำเกี่ยว cookies

--- src/index.mjs
+++ src/index.mjs
@@ -1,20 +1,21 @@
 import express from "express";
 import routes from "./routes/index.mjs";
-import { mockUsers } from "./utils/constants.mjs";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser('helloworld'));
 app.use(routes);
-
-//Get Params
-app.get("/", (req, res) => {
-  const { body } = req;
-  res.status(201).send((mockUsers[0] = { ...body }));
-});
 
 //server please use .env !!
 app.listen(PORT, () => {
   console.log(`localhost:${PORT}`);
+});
+
+//Get Params
+app.get("/", (req, res) => {
+  res.cookie("hello", "world", { maxAge: 30000, signed: true });
+  res.status(201).send({ msg: "hello" });
 });
--- src/routes/products.mjs
+++ src/routes/products.mjs
@@ -3,8 +3,14 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/api/products", (req, res) => {
+  console.log(req.headers.cookie);
+  console.log(req.cookies);
+  console.log(req.signedCookies.hello);
+  if (req.signedCookies.hello && req.signedCookies.hello === "world") {
     return res.status(200).send([{ id: 1, name: "Pepsi", pice: "50 Bath" }]);
-  });
-
+  } else {
+    return res.status(403).send({ msg: "Sorry. You need the correct cookie" });
+  }
+});
 
 export default router;
