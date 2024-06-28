30edf4aea927fee84d8f23717fd1285a4d075f66
JameN9 <jamedas3654@gmail.com>
2024-06-28 13:42:32 +0700

    เรียนรู้เกี่ยวกับ Session ต้องศึกษาเพิ่มเติม

--- src/index.mjs
+++ src/index.mjs
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { response } from "express";
 import routes from "./routes/index.mjs";
 import cookieParser from "cookie-parser";
 import session from "express-session";
+import { mockUsers } from "./utils/constants.mjs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -33,4 +34,47 @@ app.get("/", (req, res) => {
   console.log(req.session.id);
   req.session.visited = true; //แก้การส่ง session id ซ้ำ
   res.status(201).send({ msg: "hello" });
+});
+
+app.post("/api/auth", (req, res) => {
+  const {
+    body: { username, password },
+  } = req;
+  const findUser = mockUsers.find((user) => user.username === username);
+  if (!findUser || findUser.password !== password) {
+    return res.status(401).send({ msg: "BAD CREDENTIALS" });
+  }
+  req.session.user = findUser;
+  return res.status(200).send(findUser);
+});
+
+app.get("/api/auth/status", (req, res) => {
+  req.sessionStore.get(req.sessionID, (err, session) => {
+    console.log(session);
+  });
+  return req.session.user
+    ? res.status(200).send(req.session.user)
+    : res.status(400).send({ msg: "No Authenticated" });
+});
+
+app.post("/api/cart", (req, res) => {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+  const { body: item } = req;
+  const { cart } = req.session;
+
+  if (cart) {
+    cart.push(item);
+  } else {
+    req.session.cart = [item];
+  }
+  return res.status(201).send(item);
+});
+
+app.get("/api/cart", (req, res) => {
+  if (!req.session.user) {
+    return res.sendStatus(401);
+  }
+  return res.send(req.session.cart ?? []);
 });
